a71ddb93cf802f5db05bfdd99c8e17fdefd54d7d
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2021-05-01 22:13:53 -0300

    first commit

---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s.  I'm %s.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading file:", err)
+	}
+
+	fmt.Fprintf(w, "My Family %s.", string(data))
+}
